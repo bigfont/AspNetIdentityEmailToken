833fc51de3bd8d50a58ac4a2557d8dcb2caa5a09
Shaun Luttin <shaunluttin@gmail.com>
2015-04-08 17:32:40 -0700

    Use GetDataProtectionProvider()

--- AspNetIdentityEmailToken/App_Start/Startup.Auth.cs
+++ AspNetIdentityEmailToken/App_Start/Startup.Auth.cs
@@ -5,13 +5,20 @@ using Microsoft.Owin.Security.Cookies;
 using IdentitySample.Models;
 using Owin;
 using System;
+using Microsoft.Owin.Security.DataProtection;
 
 namespace IdentitySample
 {
     public partial class Startup
     {
+        // Use a static provider, lest we receive the dreaded error:
+        // The data protection operation was unsuccessful. 
+        // This may have been caused by not having the user profile loaded for the current thread's user context, which may be the case when the thread is impersonating.
+        public static IDataProtectionProvider DataProtectionProvider { get; private set; }
+
         public void ConfigureAuth(IAppBuilder app)
         {
+            DataProtectionProvider = app.GetDataProtectionProvider();
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
         }
--- AspNetIdentityEmailToken/Startup.cs
+++ AspNetIdentityEmailToken/Startup.cs
@@ -5,14 +5,8 @@ namespace IdentitySample
 {
     public partial class Startup
     {
-        // Use a static provider, lest we receive the dreaded error:
-        // The data protection operation was unsuccessful. 
-        // This may have been caused by not having the user profile loaded for the current thread's user context, which may be the case when the thread is impersonating.
-        public static IDataProtectionProvider DataProtectionProvider { get; private set; }
-
         public void Configuration(IAppBuilder app)
         {
-            DataProtectionProvider = new DpapiDataProtectionProvider("WebApp2015");
             ConfigureAuth(app);
         }
     }
